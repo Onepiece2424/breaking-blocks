39a4d179431b4eb85fd77cf62a83c41b33640c58
永野　理緒人 <nagano@fundely.co.jp>
2023-12-01 14:49:24 +0900

    add:ボールの作成

--- app/components/Block/index.jsx
+++ app/components/Block/index.jsx
@@ -1,8 +1,39 @@
 "use client";
 
+import { useEffect, useRef } from 'react';
+
 const BlockMain = () => {
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    // 描画の処理
+    ctx.beginPath();
+    ctx.rect(20, 40, 50, 50);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.rect(160, 10, 100, 40);
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.stroke();
+    ctx.closePath();
+  }, []);
+
   return (
-    <div>BlockMain</div>
+    <>
+     <canvas ref={canvasRef} width={300} height={200} />
+    </>
   )
 }
 
