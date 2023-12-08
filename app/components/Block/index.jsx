474afa30cc702faa761f57f557a9253040f68b0d
永野　理緒人 <nagano@fundely.co.jp>
2023-12-08 15:44:20 +0900

    refactor:useEffect内に定義していた関数を外に定義

--- app/components/Block/index.jsx
+++ app/components/Block/index.jsx
@@ -19,81 +19,85 @@ const BlockMain = () => {
     },
   });
 
-  useEffect(() => {
+  const drawBall = (ctx, ball) => {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  const drawPaddle = (ctx, paddle, canvas) => {
+    ctx.beginPath();
+    ctx.rect(paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  const draw = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    const { ball, paddle } = gameRef.current;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall(ctx, ball);
+    drawPaddle(ctx, paddle, canvas);
+
+    if (
+      ball.x + ball.dx > canvas.width - ball.radius ||
+      ball.x + ball.dx < ball.radius
+    ) {
+      ball.dx = -ball.dx;
+    }
+
+    if (ball.y + ball.dy < ball.radius) {
+      ball.dy = -ball.dy;
+    } else if (ball.y + ball.dy > canvas.height - ball.radius) {
+      // パドルに当たった場合
+      if (
+        ball.x > paddle.x &&
+        ball.x < paddle.x + paddle.width
+      ) {
+        ball.dy = -ball.dy;
+      } else {
+        // ゲームオーバーの処理（ここでは初期位置にボールを戻す）
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height - 30;
+      }
+    }
+
+    if (paddle.x + ball.dx > 0 && paddle.x + paddle.width + ball.dx < canvas.width) {
+      paddle.x += ball.dx;
+    }
+
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    requestAnimationFrame(draw);
+  };
+
+  const mouseMoveHandler = (e) => {
+    const canvas = canvasRef.current;
+    const { paddle } = gameRef.current;
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+      paddle.x = relativeX - paddle.width / 2;
+    }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
     const { ball, paddle } = gameRef.current;
 
     ball.x = canvas.width / 2;
     ball.y = canvas.height - 30;
     paddle.x = (canvas.width - paddle.width) / 2;
 
-    const drawBall = () => {
-      ctx.beginPath();
-      ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
-    };
-
-    const drawPaddle = () => {
-      ctx.beginPath();
-      ctx.rect(paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
-    };
-
-    const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawBall();
-      drawPaddle();
-
-      if (
-        ball.x + ball.dx > canvas.width - ball.radius ||
-        ball.x + ball.dx < ball.radius
-      ) {
-        ball.dx = -ball.dx;
-      }
-
-      if (ball.y + ball.dy < ball.radius) {
-        ball.dy = -ball.dy;
-      } else if (ball.y + ball.dy > canvas.height - ball.radius) {
-        // パドルに当たった場合
-        if (
-          ball.x > paddle.x &&
-          ball.x < paddle.x + paddle.width
-        ) {
-          ball.dy = -ball.dy;
-        } else {
-          // ゲームオーバーの処理（ここでは初期位置にボールを戻す）
-          ball.x = canvas.width / 2;
-          ball.y = canvas.height - 30;
-        }
-      }
-
-      if (paddle.x + ball.dx > 0 && paddle.x + paddle.width + ball.dx < canvas.width) {
-        paddle.x += ball.dx;
-      }
-
-      ball.x += ball.dx;
-      ball.y += ball.dy;
-
-      requestAnimationFrame(draw);
-    };
-
-    const mouseMoveHandler = (e) => {
-      const relativeX = e.clientX - canvas.offsetLeft;
-      if (relativeX > 0 && relativeX < canvas.width) {
-        paddle.x = relativeX - paddle.width / 2;
-      }
-    };
-
     canvas.addEventListener("mousemove", mouseMoveHandler);
 
     draw();
 
-    // コンポーネントがアンマウントされたらイベントリスナーを削除
     return () => {
       canvas.removeEventListener("mousemove", mouseMoveHandler);
     };
