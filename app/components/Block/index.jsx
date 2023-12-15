e134ebccf606e9801af7be111d5b0ee3a96fdc92
rioto.nagano <kingdom950927@gmail.com>
2023-12-16 06:11:35 +0900

    feat:ボールが当たった後の角度を変更する機能を実装

--- app/components/Block/index.jsx
+++ app/components/Block/index.jsx
@@ -53,59 +53,68 @@ const BlockMain = () => {
 
   // ゲームのアニメーション（ボールの動き、衝突判定など）
   const draw = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const { ball, paddle, blocks } = gameRef.current;
+  const canvas = canvasRef.current;
+  const ctx = canvas.getContext('2d');
+  const { ball, paddle, blocks } = gameRef.current;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall(ctx, ball);
-    drawPaddle(ctx, paddle, canvas);
-    drawBlocks(ctx, blocks);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall(ctx, ball);
+  drawPaddle(ctx, paddle, canvas);
+  drawBlocks(ctx, blocks);
 
+  if (
+    ball.x + ball.dx > canvas.width - ball.radius ||
+    ball.x + ball.dx < ball.radius
+  ) {
+    ball.dx = -ball.dx;
+  }
+
+  if (ball.y + ball.dy < ball.radius) {
+    ball.dy = -ball.dy;
+  } else if (ball.y + ball.dy > canvas.height - ball.radius) {
+    // パドルに当たった場合
     if (
-      ball.x + ball.dx > canvas.width - ball.radius ||
-      ball.x + ball.dx < ball.radius
+      ball.x > paddle.x &&
+      ball.x < paddle.x + paddle.width
     ) {
-      ball.dx = -ball.dx;
-    }
+      // パドルの中央に対するボールの相対的な位置を計算
+      const relativePosition = (ball.x - (paddle.x + paddle.width / 2)) / (paddle.width / 2);
 
-    if (ball.y + ball.dy < ball.radius) {
-      ball.dy = -ball.dy;
-    } else if (ball.y + ball.dy > canvas.height - ball.radius) {
-      // パドルに当たった場合
+      // 反射角度を計算
+      const reflectionAngle = relativePosition * Math.PI / 4; // 例として PI / 4 をかけていますが、適切な値を調整してください
+
+      // ボールの移動方向を更新
+      ball.dx = Math.sin(reflectionAngle) * 2;
+      ball.dy = -Math.cos(reflectionAngle) * 2;
+    } else {
+      // ゲームオーバーの処理
+      alert('GAME OVER');
+      document.location.reload();
+      return;
+    }
+  }
+
+  // ブロックとの当たり判定
+  blocks.forEach((block) => {
+    if (!block.isDestroyed) {
       if (
-        ball.x > paddle.x &&
-        ball.x < paddle.x + paddle.width
+        ball.x > block.x &&
+        ball.x < block.x + block.width &&
+        ball.y > block.y &&
+        ball.y < block.y + block.height
       ) {
+        block.isDestroyed = true;
         ball.dy = -ball.dy;
-      } else {
-        // ゲームオーバーの処理
-        alert('GAME OVER')
-        document.location.reload();
-        return;
       }
     }
+  });
 
-    // ブロックとの当たり判定
-    blocks.forEach((block) => {
-      if (!block.isDestroyed) {
-        if (
-          ball.x > block.x &&
-          ball.x < block.x + block.width &&
-          ball.y > block.y &&
-          ball.y < block.y + block.height
-        ) {
-          block.isDestroyed = true;
-          ball.dy = -ball.dy;
-        }
-      }
-    });
+  ball.x += ball.dx;
+  ball.y += ball.dy;
 
-    ball.x += ball.dx;
-    ball.y += ball.dy;
+  requestAnimationFrame(draw);
+};
 
-    requestAnimationFrame(draw);
-  };
 
   // マウスの位置に応じてパドルの位置を更新
   const mouseMoveHandler = (e) => {
